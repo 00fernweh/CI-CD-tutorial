9e22a018713004e3f3bf612dd87080103e54dffa
Zeynep Ecem Colak <z.colak@tu-braunschweig.de>
2025-03-25 18:05:37 +0100

    flask nad dockerfile

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def merhaba():
-    return "Merhaba CI/CD!"
+def Hi():
+    return "Hi CI/CD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
